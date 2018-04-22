a7c3d5e49facd92c102b2852a2098c6983cda0e5
Your Namedd6777 <pawan6777@gmail.com>
2018-04-22 23:02:28 +0530

    bootstrap

--- book-app/src/App.js
+++ book-app/src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import 'font-awesome/css/font-awesome.min.css';;
+ 
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container-fluid padding-0 bg-indigo"> 
+          <div className="container padding-all">
+              <h2 className="white"><i className="fa fa-book"> </i>My Books</h2>
+          </div>
+
+
+
       </div>
+
+
     );
   }
 }
